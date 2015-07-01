50096daad35b9ffd3b592bef5ebd453ad44e5426
Nicolas Labrot <nithril@gmail.com>
2015-07-01 21:50:14 +0200

    initial commit

---
+++ projects.groovy
@@ -0,0 +1,12 @@
+    [
+      {
+        "id": "application",
+        "name": "Generated Application",
+        "scm": "https://github.com/nithril/article-continuous-release.git"
+      },
+      {
+        "id": "dependency",
+        "name": "Generated Dependency",
+        "scm": "https://github.com/nithril/article-continuous-release.git"
+      }
+    ]
